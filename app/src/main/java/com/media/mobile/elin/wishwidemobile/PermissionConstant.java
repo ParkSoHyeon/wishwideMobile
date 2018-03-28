d7ee1ee5f8c5e23cc64dabe4f3ea91de168ccc33
dso753 <dso753@naver.com>
2018-03-28 20:06:38 +0900

    환경설정 기능 추가

---
+++ app/src/main/java/com/media/mobile/elin/wishwidemobile/PermissionConstant.java
@@ -0,0 +1,12 @@
+package com.media.mobile.elin.wishwidemobile;
+
+import android.Manifest;
+
+public interface PermissionConstant {
+    public static final int STORAGE_PERMISSION = 0;
+    public static final int ROCATION_FIND_PERMISSION = 1;
+    public static final int NEARBY_BEACON_FIND_PERMISSION = 2;
+    public static final int CONTACT_PERMISSION = 3;
+    public static final int CALL_PERMISSION = 4;
+    public static final int GAME_START_PERMISSION = 5;
+}
--- app/src/main/java/com/media/mobile/elin/wishwidemobile/SharedPreferencesConstant.java
+++ app/src/main/java/com/media/mobile/elin/wishwidemobile/SharedPreferencesConstant.java
@@ -5,7 +5,7 @@ public interface SharedPreferencesConstant {
     public static final String WIDE_CUSTOMER_NO_KEY = "WIDE_CUSTOMER_NO";
     public static final String WIDE_CUSTOMER_PHONE_KEY = "WIDE_CUSTOMER_PHONE";
     public static final String WIDE_CUSTOMER_EMAIL_KEY = "WIDE_CUSTOMER_EMAIL";
-    public static final String MWIDE_CUSTOMER_NAME_KEY = "WIDE_CUSTOMER_NAME";
-    public static final String MWIDE_CUSTOMER_BIRTH_KEY = "WIDE_CUSTOMER_BIRTH";
+    public static final String WIDE_CUSTOMER_NAME_KEY = "WIDE_CUSTOMER_NAME";
+    public static final String WIDE_CUSTOMER_BIRTH_KEY = "WIDE_CUSTOMER_BIRTH";
     public static final String WIDE_CUSTOMER_SEX_KEY = "WIDE_CUSTOMER_SEX";
 }
